ffc23b46dcf199e40aeef7aa238c2b90453d750b
jesseclldr <jdragstra@protoandgo.com>
2021-11-11 12:15:02 +0100

    update

--- hey.js
+++
@@ -1,9 +0,0 @@
-import { OnshapeClient } from "./src/client/Onshape";
-
-
-
-const client = new OnshapeClient();
-const putilla = async function(){
-   const response = await  client.createDocument({hey:'hey como estas'})
-   console.log(response)
-}
